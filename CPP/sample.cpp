dc91f74807eae3bef60bd921792709f3addba670
Souvik Nandi <souviknandi707@gmail.com>
2022-10-10 22:59:13 +0530

    Update sample.cpp

--- CPP/sample.cpp
+++ CPP/sample.cpp
@@ -1,1 +1,12 @@
 
+int fibseries(int a, int b, int n)
+{
+    int sum = 0;
+    for (int i = 1; i <= (n - 2); i++)
+    {
+        sum = a + b;
+        cout << sum << " ";
+        a = b;
+        b = sum;
+    }
+}
