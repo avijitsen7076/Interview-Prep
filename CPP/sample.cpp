eda4d94cf36faab37ef3193102f06d21dad93f81
Souvik Nandi <souviknandi707@gmail.com>
2022-10-10 23:00:33 +0530

    Update sample.cpp

--- CPP/sample.cpp
+++ CPP/sample.cpp
@@ -1,1 +1,27 @@
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
 
+    while (i <= n)
+    {
+        int number = i;
+        int space = i - 1;
+        while (space)
+        {
+            cout << " ";
+            space--;
+        }
+        int j = 1;
+
+        while (j <= (n - i + 1))
+        {
+            cout << number;
+            number++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+}
