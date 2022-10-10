9488dff2338e0a803126f212f85e34a97441f522
Souvik Nandi <souviknandi707@gmail.com>
2022-10-10 22:52:28 +0530

    Update sample.cpp

--- CPP/sample.cpp
+++ CPP/sample.cpp
@@ -1,1 +1,12 @@
+int reverse(int n)
+{ // 123//
+    int rev = 0, lastdigit = 0;
+    while (n > 0)
+    {                               // 12//1
+        lastdigit = n % 10;         // 3//2//1
+        rev = 10 * rev + lastdigit; // 3//32//321
+        n /= 10;                    // 12//1
+    }
+    return rev;
+}
 
