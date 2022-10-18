9ec0585c75006fc95de665dff3927e6f0c4d7793
Souvik Nandi <souviknandi707@gmail.com>
2022-10-19 00:58:49 +0530

    FindArrayReverse.cpp

---
+++ CPP/FindArrayReverse.cpp
@@ -0,0 +1,12 @@
+
+void FindArrayReverse(int arr[], int n)
+{
+    int start = 0;
+    int end = n - 1;
+    while (start <= end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
